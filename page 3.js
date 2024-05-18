0744798cbf1d9a55be5600cc1abded436a5d09cd
Bailse <sua88.pattaraphol@gmail.com>
2024-05-18 14:24:19 +0700

    first commit

---
+++ page 3.js
@@ -0,0 +1,102 @@
+
+function myFunction()
+{
+let a=document.getElementById("text_a").value;
+if (a=="hello ")
+  {
+    window.location.href = "page 4.html"  }
+else if(a==""){
+    window.location.href = "page 3.html"
+}
+else if(a==""){
+  window.location.href = "" //1//
+}else if(a==""){
+  window.location.href = ""//3//
+}else if(a=="lnwslot888"){
+  window.location.href = "4dum.html"//4//
+}else if(a=="chaishooter.com"){
+  window.location.href = ""//5//
+}else if(a=="doctorodie.gay"){
+  window.location.href = "6rattajeaw.html" //6//
+}else if(a==""){           
+  window.location.href = ""//7//
+}else if(a=="jomtap_on_vacation"){          
+  window.location.href = ""//8//
+}else if(a=="tiktokboyz"){
+  window.location.href = ""//9//
+}else if(a=="holymf_bigass_of_DSN"){
+  window.location.href = ""//10//
+}else if(a=="11"){
+  window.location.href = ""//11//
+}else if(a=="12"){
+  window.location.href = ""//12//
+}else if(a=="13"){
+  window.location.href = ""//13//
+}else if(a=="14"){
+  window.location.href = ""//14//
+}else if(a=="#เจ้าแม่ทุกสถาบัน"){ //15//
+  window.location.href = ""
+}else if(a==""){//16//
+  window.location.href = ""
+}else if(a==""){//17//
+  window.location.href = ""
+}else if(a=="หลอนเต็มพิกัดกะชินนาม่อน"){//18//
+  window.location.href = ""//19//
+}else if(a==""){
+  window.location.href = ""//20//
+}else if(a=="aunt_noey80years"){
+  window.location.href = ""//21//
+}else if(a==""){
+  window.location.href = ""//22//
+}else if(a==""){
+  window.location.href = ""//23//
+}else if(a==""){
+  window.location.href = ""//24//
+}else if(a==""){
+  window.location.href = ""//25//
+}else if(a==""){
+  window.location.href = ""//26//
+}else if(a==""){
+  window.location.href = ""//27//
+}else if(a==""){
+  window.location.href = ""//28//
+}else if(a==""){
+  window.location.href = ""//29//
+}else if(a==""){
+  window.location.href = ""//30//
+}else if(a=="@ตื่นมาพักผ่อนบ้าง"){
+  window.location.href = ""//31//
+}else if(a==""){
+  window.location.href = ""//33//
+}else if(a==""){
+  window.location.href = ""//34//
+}else if(a==""){
+  window.location.href = ""//35//
+}else if(a==""){
+  window.location.href = ""//36//
+}else if(a==""){
+  window.location.href = ""//37//
+}else if(a==""){
+  window.location.href = ""//38//
+}else if(a==""){
+  window.location.href = ""//39//
+}else if(a==""){
+  window.location.href = ""//40//
+}else if(a==""){
+  window.location.href = ""//41//
+}else if(a=="งับงับงับงับ"){
+  window.location.href = ""//42//
+}else if(a==""){
+  window.location.href = ""//43//
+}else if(a=="Pan_zebra"){
+  window.location.href = ""//44//
+}
+else if(a=="lnwLiron4"){
+  window.location.href = ""//ตัวบัคakaเก้า//
+}
+else
+  {
+    alert ("มันผิดอะเด้");//ผิด//
+    window.location.href = "page 3.html"
+  }
+}
